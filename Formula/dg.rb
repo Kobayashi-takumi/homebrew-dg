3fe940acdd31a064f0b435a194869c0a47af16fa
Takumi Kobayashi <t.kobayashi@inside.tworks.co.jp>
2023-06-05 10:51:08 +0900

    v0.3.0

--- Formula/dg.rb
+++ Formula/dg.rb
@@ -2,8 +2,8 @@ class Dg < Formula
   desc "Dancing Gopher"
   homepage "https://github.com/Kobayashi-takumi/dancing-gopher"
   url "https://github.com/Kobayashi-takumi/dancing-gopher/releases/download/v0.2.0/dg-0.2.0-aarch64-apple-darwin.tar.gz"
-  sha256 "953580ee54ab750f90703a79f6a6edbec575c3fb23dc6bd64034723c51289f13"
-  version "0.2.0"
+  sha256 "77eb0f5153000f065fd5b25e73cf2c198f71ebf01e0d154398180c5687680b7f"
+  version "0.3.0"
 
   def install
     bin.install "dg"
